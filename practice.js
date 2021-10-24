1537be6059c3e6fc02d1c911246d088741a4c80a
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-25 00:35:12 +0530

    Data types

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
 console.log("Welcome to JavaSrcipt Practice!!..");
+let numType = 16;
+console.log("num type "+ numType);
+let lastName = "Johnson";
+console.log("string type "+ lastName);
+//Object type data
+let objectType = {firstName:"John", lastName:"Doe"};
+console.log(objectType.firstName + " and "+ objectType.lastName);
+//JavaScript Types are Dynamic
+let DynamicType;
+DynamicType = 5;
+console.log("num type: "+ DynamicType);
+DynamicType = "John";
+console.log("string type: " + DynamicType);
+//Booleans type data
+let boolx = 5;
+let booly = 5;
+let boolz = 6;
+console.log(boolx == booly);// Returns true
+console.log(booly == boolz);// Returns false
+//JavaScript Booleans
+console.log(typeof ""  );           // Returns "string"
+console.log(typeof "John");         // Returns "string"
+let car;    // Value is undefined, type is undefined
+console.log("undefined type data: " +car);
+let car1 = "";    // The value is "", the typeof is "string
+console.log("Empty type data "+car1);
