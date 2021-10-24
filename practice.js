90903afc9285b19f38f6b185a91cbc35f12ac7a4
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-25 02:04:47 +0530

    Math and Random num fun

--- practice.js
+++ practice.js
@@ -42,3 +42,19 @@ console.log(arrOfString.sort());
 let arrOfNum = [40, 1, 5, 25,];
 arrOfNum.push(21);
 console.log(arrOfNum);
+
+//he JavaScript Math object allows you to perform mathematical tasks on numbers
+console.log(Math.round(4.9));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(64));
+console.log(Math.abs(-4.7));
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+//Returns a random integer from 0 to 9:
+console.log(Math.floor(Math.random() * 10));
+//Returns a random integer from 0 to 99:
+console.log(Math.floor(Math.random() * 100));
+//Returns a random integer from 1 to 10:
+console.log(Math.floor(Math.random() * 10) + 1);
