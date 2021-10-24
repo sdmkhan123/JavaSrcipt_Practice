63b8cc6c276b6c6726321c10d6d28bea683c2e8e
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-25 01:08:09 +0530

    Function

--- practice.js
+++ practice.js
@@ -25,3 +25,11 @@ let car;    // Value is undefined, type is undefined
 console.log("undefined type data: " +car);
 let car1 = "";    // The value is "", the typeof is "string
 console.log("Empty type data "+car1);
+
+//UC-2 Functions
+let funReturn = myFunction(4, 3);
+console.log("Product of two num with local var: "+funReturn);
+function myFunction(a, b) {
+    let c = 30;
+    return a * b * c;
+}
